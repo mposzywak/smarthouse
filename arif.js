ee66e8f1451f2c5a6a98305e5fd48ec4bcb08cbc
Maciej Poszywak <maciej.poszywak@gmail.com>
2017-11-12 15:01:21 +0100

    Removed node_modules and fixed 6 -> 8 node API changes

--- arif.js
+++ arif.js
@@ -16,9 +16,29 @@ ARiF.prototype.sendCommand = function(device, command, callback) {
 	debug = this.debug;
 	debug.log(1, 'arif', 'Sending ARiF command to device devID: ' + device.devID + ' ardID: ' + 
 			device.ardID + ' IP: ' + device.IP + ' command: ' + command);
-			
+				
 	var http = require('http');
-	var ARiFRaspyClient = http.createClient(this.config.arif.port, device.IP);
+	
+	var options = {
+		hostname: device.IP,
+		port: this.config.arif.port,
+		path: '/' + device.devID + '/' + device.ardID + '/' + this.config.rcpclient.vpnID.split('-')[1] + '/' + command,
+		method: 'POST',
+		agent: false
+	};
+	
+	var req = http.request(options, function (res){
+		debug.log(1, 'arif', 'Received ARiF command resp from: ' + device.devID + ' ardID: ' + 
+			device.ardID + ' IP: ' + device.IP);
+	}).on('error', function(error) {
+		debug.log(1, 'arif', 'Error in ARiF comms with: ' + device.devID + ' ardID: ' + 
+			device.ardID + ' IP: ' + device.IP);
+	});
+	
+	req.write('');
+	req.end();
+	
+	/*var ARiFRaspyClient = http.createClient(this.config.arif.port, device.IP);
 	var request = ARiFRaspyClient.request('POST', '/' + device.devID + '/' + device.ardID + '/' + this.config.rcpclient.vpnID.split('-')[1] +
 			'/' + command, {'host': 'raspy',  'content-type': 'application/json'});
 	request.write(JSON.stringify(device), encoding='utf8'); //possibly need to escape as well? 
@@ -35,7 +55,7 @@ ARiF.prototype.sendCommand = function(device, command, callback) {
 		require('./debug.js').log(4, 'arif', 'Received STATUS: ' + response.statusCode + ' for ardID: ' + device.ardID + ' devID: ' + device.devID);
 	});
 	
-	request.end();
+	request.end(); */
 }
 
 module.exports = arif;
--- rcpclient.js
+++ rcpclient.js
@@ -14,6 +14,34 @@ var rcpclient = new RCPClient();
 RCPClient.prototype.sendDeviceStatus = function(device) {
 	// Create HTTP client
 	var http = require('http');
+	
+	require('./debug.js').log(4, 'rcpclient', 'Sending device status of ardID: ' 
+			+ device.ardid + ' devID: ' + device.devid);
+
+	var options = {
+		hostname: this.config.rcpclient.host,
+		port: this.config.rcpclient.port,
+		path: '/device',
+		method: 'POST',
+		agent: false,
+		headers: {
+			'iot-raspyid' : this.config.rcpclient.vpnID, 
+			'iot-vpnkey' : this.config.rcpclient.vpnkey
+		}
+	};
+	
+	var req = http.request(options, function (res){
+		require('./debug.js').log(4, 'rcpclient', 'Received STATUS: ' + res.statusCode + 
+				' for ardid: ' + device.ardid + ' devid: ' + device.devid);
+	}).on('error', function(error) {
+		require('./debug.js').log(1, 'rcpclient', 'Failed to establish connection with the cloud: ', 
+				error.message);
+	});
+	
+	req.write(JSON.stringify(device),encoding='utf8');
+	req.end();
+	
+	/*
 	var rcpclient = http.createClient(this.config.rcpclient.port, this.config.rcpclient.host);
 	var request = rcpclient.request('POST', '/device', {'host': 'sever',  'content-type': 'application/json', 
 			'iot-raspyid' : this.config.rcpclient.vpnID, 'iot-vpnkey' : this.config.rcpclient.vpnkey});
@@ -27,7 +55,7 @@ RCPClient.prototype.sendDeviceStatus = function(device) {
 		require('./debug.js').log(4, 'rcpclient', 'Received STATUS: ' + response.statusCode + ' for ardid: ' + device.ardid + ' devid: ' + device.devid);
 	});
 	
-	request.end();
+	request.end();*/
 }
 
 module.exports = rcpclient;
